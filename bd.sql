3e6bd3bcd4a73036a37a4d431530c8122597fe7d
João Victor <joaosantoos@aol.com>
2021-05-10 16:21:43 -0300

    kauzedaaa ta ai meu pit

--- bd.sql
+++ bd.sql
@@ -3,17 +3,17 @@ Create database Monitoramento_Saude;
 use Monitoramento_Saude;
 
 create table tb_usuario(
-
 id_usuario int primary key identity(1,1),
 nome_usuario varchar(100),
 sobrenome_usuario varchar(100),
 email varchar(100),
 data_Nascimento DATE,
+--senha_usuario varchar(50), 
 sexo char(2),
 check(sexo in ('M','F'))
 )
 
-insert into tb_usuario values('João ','Victor','joaosantoo@aol.com','26/01/2004','M');
+insert into tb_usuario values('João ','Victor','joao@gmail.com','26/01/2004','M');
 insert into tb_usuario values('Kaue ','Soares','kauesoares@gmail.com','01/01/2001','M');
 
 select * from tb_usuario
@@ -24,8 +24,8 @@ id_servico int primary key identity(1,1),
 nome_servico varchar(100),
 descricao varchar(100));
 
-insert into tb_servico values('IMC', 'Índice de Massa Corporal');
-insert into tb_servico values('Sono', 'Monitoramento de Sono');
+insert into tb_servico values('IMC', 'Índice de Massa Corporal',);
+insert into tb_servico values('Sono', 'Monitoramento de Sono',);
 
 
 create table tb_usuario_servico(
@@ -57,3 +57,5 @@ FROM
  tb_servico INNER JOIN
  tb_usuario_servico ON tb_servico.id_servico = tb_usuario_servico.id_servico INNER JOIN
  tb_usuario ON tb_usuario_servico.id_usuario = tb_usuario.id_usuario
+
+ -- select avg(horario_sono_fim - horario_sono_ini) from tb_usuario_servico 
